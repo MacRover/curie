55a79e4ef1d9c69937693c780f22b20f291684d2
chosterto <johnnychox@gmail.com>
2026-01-03 23:41:55 -0500

    Hardware interface + drivetrain implementation

--- curie_ws/src/curie_hw_control/interfaces/hardware.hpp
+++ curie_ws/src/curie_hw_control/interfaces/hardware.hpp
@@ -1,33 +1,34 @@
 #pragma once
 #include <cstdint>
+#include <memory>
 
 namespace hardware 
 {
     class HardwareInterface
     {
     public:
-        virtual ~HardwareInterface() = default;
-
         /**
          * @brief Initialize hardware interface. Anything that needs to be setup should be done here
-         * @return 0 if initialization was successful, negative error code otherwise (<= 0)
          */
         virtual uint8_t initialize() = 0;
         /**
          * @brief Shutdown hardware interface. Anything that needs to be cleaned up before exiting any 
          * process should be done here
-         * @return 0 if shutdown was successful, negative error code otherwise (<= 0)
          */
         virtual uint8_t shutdown() = 0;
         /**
          * @brief read data from hardware
          * @return 0 if read was successful, negative error code otherwise (<= 0)
          */
-        virtual uint8_t read() = 0;
+        virtual uint8_t read(std::shared_ptr<void> data) = 0;
         /**
          * @brief write data to hardware
          * @return 0 if write was successful, negative error code otherwise (<= 0)
          */
-        virtual uint8_t write() = 0;
+        virtual uint8_t write(std::shared_ptr<void> data) = 0;
+        /**
+         * @brief main loop for hardware interface
+         */
+        virtual void run() = 0;
     };
 }
