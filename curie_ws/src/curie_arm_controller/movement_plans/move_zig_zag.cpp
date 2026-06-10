a8f70122148eec218598436d49644ea387fd5bad
chosterto <johnnychox@gmail.com>
2026-06-10 15:43:33 -0400

    Add zig zag movement pattern

--- curie_ws/src/curie_arm_controller/movement_plans/move_zig_zag.cpp
+++ curie_ws/src/curie_arm_controller/movement_plans/move_zig_zag.cpp
@@ -13,57 +13,43 @@ int main(int argc, char** argv)
   std::thread spinner([&executor]() { executor.spin(); });
 
   moveit::planning_interface::MoveGroupInterface move_group(node, "arm");
-  moveit_msgs::msg::RobotTrajectory trajectory;
-  std::vector<geometry_msgs::msg::Pose> waypoints;
-  geometry_msgs::msg::Pose target_pose;
-  double fraction;
 
-  move_group.setGoalTolerance(0.02);
+  move_group.setGoalTolerance(0.001);
   move_group.setPlanningTime(10.0);
 
+  const double delta = 0.2;
+  double x_delta_arr[] = {0, delta, -delta, delta, -delta};
+  double z_delta_arr[] = {0, 0,     delta,  0,     -delta};
+  bool success;
+
+  geometry_msgs::msg::Pose target_pose;
   target_pose.orientation.x = 0.5;
   target_pose.orientation.y = -0.5;
   target_pose.orientation.z = -0.5;
   target_pose.orientation.w = 0.5;
-  target_pose.position.x = 0.376;
-  target_pose.position.y = -0.056;
-  target_pose.position.z = 0.3;
+  target_pose.position.x = 0.3;
+  target_pose.position.y = -0.0555596;
+  target_pose.position.z = 0.2;
 
-  move_group.setPoseTarget(target_pose);
-
-  moveit::planning_interface::MoveGroupInterface::Plan plan;
-  bool success = (move_group.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
-
-  if (success)
+  for (size_t i = 0; i < 5; i++)
   {
-    RCLCPP_INFO(node->get_logger(), "Plan successful, executing...");
-    move_group.execute(plan);
-  }
-  else
-  {
-    RCLCPP_ERROR(node->get_logger(), "Planning failed");
-    goto cleanup;
-  }
-
-  move_group.clearPoseTargets();
-  move_group.setStartStateToCurrentState();
-  target_pose.position.x += 0.2;
-  waypoints.push_back(target_pose);
-
-  fraction = move_group.computeCartesianPath(waypoints, 0.01, 0.0, trajectory);
-
-  if (fraction > 0.0)
-  {
-    RCLCPP_INFO(node->get_logger(), "Cartesian path computed successfully (%.2f%% achieved)", fraction * 100.0);
-    move_group.execute(trajectory);
-  }
-  else
-  {
-    RCLCPP_ERROR(node->get_logger(), "Failed to compute Cartesian path");
-    goto cleanup;
+    target_pose.position.x += x_delta_arr[i];
+    target_pose.position.z += z_delta_arr[i];
+    move_group.setPoseTarget(target_pose);
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    success = (move_group.plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
+    if (success)
+    {
+      RCLCPP_INFO(node->get_logger(), "Plan successful, executing...");
+      move_group.execute(plan);
+    }
+    else
+    {
+      RCLCPP_ERROR(node->get_logger(), "Planning failed");
+      break;
+    }
   }
 
-cleanup:
   rclcpp::shutdown();
   spinner.join();
   return 0;
