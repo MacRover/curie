aaf4b506e8e600b93867c00faeeb09ec072282f3
LfovS9 <ansonliang001@gmail.com>
2026-07-20 13:06:52 -0400

    add xbox controller interface to base.cpp

--- curie_ws/src/curie_base/include/curie_base/base.hpp
+++ curie_ws/src/curie_base/include/curie_base/base.hpp
@@ -9,6 +9,8 @@
 #include "std_srvs/srv/set_bool.hpp"
 #include "std_srvs/srv/trigger.hpp"
 #include "controller_manager_msgs/srv/switch_controller.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "control_msgs/action/gripper_command.hpp"
 
 typedef enum
 {
@@ -18,6 +20,9 @@ typedef enum
 
 namespace base
 {
+    using GripperCommand = control_msgs::action::GripperCommand;
+    using GripperGoalHandle = rclcpp_action::ClientGoalHandle<GripperCommand>;
+    
     class Basestation : public rclcpp::Node
     {
     public:
@@ -28,6 +33,10 @@ namespace base
         double _map(double value, double istart, double iend, double ostart, double oend);
         void _handle_servo_state(bool enable, bool disable);
         void _handle_robot_state(bool enable, bool disable);
+        void _handle_gripper_state(bool open, bool close);
+        void _send_gripper_goal(double target_position);
+        void _handle_gripper_result(const GripperGoalHandle::WrappedResult & result);
+        void _send_gripper_hold_goal(double hold_position);
         rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_drive_sub_;
         rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr arm_drive_sub_;
         rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr drive_pub_;
@@ -37,6 +46,9 @@ namespace base
         rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr servo_enable_client_, servo_disable_client_;
         rclcpp::Client<controller_manager_msgs::srv::SwitchController>::SharedPtr controller_client_;
         rclcpp::TimerBase::SharedPtr state_timer_, servo_timer_;
+        rclcpp_action::Client<GripperCommand>::SharedPtr gripper_client_;
+
+        GripperGoalHandle::SharedPtr gripper_hold_goal_handle_;
 
         geometry_msgs::msg::TwistStamped cmd_vel_msg_;
         geometry_msgs::msg::TwistStamped servo_cmd_msg_;
@@ -49,9 +61,17 @@ namespace base
         double max_linear_speed;
         double max_angular_speed;
         double turbo_multiplier;
+        double gripper_open_position;
+        double gripper_close_position;
+        double gripper_max_effort;
         std::vector<double> arm_max_speeds;
-        bool servo_request_handled, request_handled;
-        bool servo_button_pressed, button_pressed;
+        bool servo_request_handled;
+        bool request_handled;
+        bool servo_button_pressed;
+        bool button_pressed;
+        bool gripper_open_button_pressed;
+        bool gripper_close_button_pressed;
+        bool gripper_request_active;
         bool servoing;
         
         ArmMode arm_mode;
